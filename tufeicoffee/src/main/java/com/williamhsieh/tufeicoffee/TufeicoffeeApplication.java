87a349356859cf2affee3732f7a8f87e08b9d926
William Hsieh <best5946@gmail.com>
2025-08-18 08:32:44 +0800

    初始化 Spring Boot project

---
+++ tufeicoffee/src/main/java/com/williamhsieh/tufeicoffee/TufeicoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.williamhsieh.tufeicoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TufeicoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TufeicoffeeApplication.class, args);
+    }
+
+}
---
+++ tufeicoffee/src/test/java/com/williamhsieh/tufeicoffee/TufeicoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamhsieh.tufeicoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TufeicoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
