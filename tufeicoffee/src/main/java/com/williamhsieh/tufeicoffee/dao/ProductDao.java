6a0f990e877650f9e37d965d2dc9aa92e4e61a26
謝文育 <best5946@gmail.com>
2025-08-18 19:46:16 +0800

    Delete tufeicoffee directory

--- tufeicoffee/src/main/java/com/williamhsieh/tufeicoffee/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.williamhsieh.tufeicoffee.dao;
-
-import com.williamhsieh.tufeicoffee.model.Product;
-
-public interface ProductDao {
-
-    Product getProductById(Integer productId);
-
-}
--- tufeicoffee/src/main/java/com/williamhsieh/tufeicoffee/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.williamhsieh.tufeicoffee.service;
-
-import com.williamhsieh.tufeicoffee.model.Product;
-
-public interface ProductService {
-
-    Product getProductById(Integer productId);
-
-}
