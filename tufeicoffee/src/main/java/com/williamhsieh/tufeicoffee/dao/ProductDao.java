2aeda98141a26b1797be7448a6afb0fc4224d4e4
William Hsieh <best5946@gmail.com>
2025-08-18 15:24:10 +0800

    商品功能 - 實作查詢商品

---
+++ tufeicoffee/src/main/java/com/williamhsieh/tufeicoffee/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.williamhsieh.tufeicoffee.dao;
+
+import com.williamhsieh.tufeicoffee.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+
+}
---
+++ tufeicoffee/src/main/java/com/williamhsieh/tufeicoffee/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.williamhsieh.tufeicoffee.service;
+
+import com.williamhsieh.tufeicoffee.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+
+}
